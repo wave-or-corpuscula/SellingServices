aab9c19d4e9ef8dd5d81f3a9f784eb4b1a22e1ac
wave-or-corpuscula <ninzyago@gmail.com>
2024-05-22 10:35:49 +0300

    trying to use wtfomrs

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request, flash, redirect, url_for
+from werkzeug.security import generate_password_hash
+
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "sjdFODJdsojfsodfjPFJdjs546sdfsoidfjPfj"
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        fullname = request.form['fullname']
+        phone = request.form['phone']
+        address = request.form['address']
+
+        print(username, password, fullname, phone, address)
+
+        # TODO: Сделать проверку, есть ли уже такой пользователь, и если есть, то выдать flash об ошибке
+        
+        # hashed_password = generate_password_hash(password, method='sha256')
+        # new_user = User(username=username, password=hashed_password, fullname=fullname, phone=phone, address=address)
+        
+        # db.session.add(new_user)
+        # db.session.commit()
+        
+        flash('Registration successful! You can now log in.', "success")
+        return redirect(url_for('login'))
+    return render_template('auth_register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        print(username, password)
+        # user = User.query.filter_by(username=username).first()
+        # if user and check_password_hash(user.password, password):
+        #     login_user(user)
+        # return redirect(url_for('dashboard'))
+        # else:
+        #     flash('Invalid username or password')
+    return render_template('auth_login.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
